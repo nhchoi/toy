3c9422b7c333bbb1e83d75c5173162dd359681f9
nhchoi <nhchoi@cookapps.com>
2016-01-26 17:02:56 +0900

    b commit

---
+++ b.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nhchoi
+ * Date: 16. 1. 26.
+ * Time: 17:00
+ */
+
+function xxx() {
+    return mt_rand(0, 10);
+}
+
+
+echo time() . " : " . xxx()
+
+?>
